c6ee6c36ef49ea815cf91f8daff4a75d9b33f037
Backerich <25672920+Backerich@users.noreply.github.com>
2020-03-21 12:42:44 +0100

    Basic CRUD

--- db/db.go
+++ db/db.go
@@ -4,44 +4,149 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net"
 	"os"
+	"time"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3" // Need to be like this
 )
 
-// Praxis
-// Resource
-
-func IsThisRight() {
-	fmt.Println("TEST")
+// Medicine is the stuff that makes you healthy
+type Medicine struct {
+	UUID       string    `json:"uuid"`
+	Title      string    `json:"title"`
+	Desciption string    `json:"desciption"`
+	CreatedAt  time.Time `json:"createdAt"`
+	Owner      string    `json:"owner"`
 }
 
-func CreateMedicine() {
-	os.Remove("./meds.db")
-	db, err := sql.Open("sqlite3", "./meds.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+// Packet for transmitting between Peers
+type Packet struct {
+	UUID string
+	Type string
+	data []byte
+}
 
-	sqlStmt := `
-	create table med (id integer not null primary key, name text);
-	delete from med;
-	`
+// Peer for caching the Doctors office
+type Peer struct {
+	IP       net.IP
+	Port     uint32
+	LastSeen time.Time
+}
 
-	_, err = db.Exec(sqlStmt)
-	if err != nil {
-		log.Printf("%q: %s\n", err, sqlStmt)
-		return
-	}
-
+// AddMedicine adds a Medicine Object to the Database
+func (med *Medicine) AddMedicine(db *sql.DB) {
 	tx, err := db.Begin()
 	if err != nil {
 		log.Fatal(err)
 	}
-	stmt, err := tx.Prepare("insert into med(id, name) values(0, 'test')")
+	stmt, err := tx.Prepare("insert into med(id, title, desciption, createdAt, owner) values(?, ?, ?, ?, ?)")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer stmt.Close()
+
+	// _, err = stmt.Exec(i, fmt.Sprintf("こんにちわ世界%03d", i))
+	_, err = stmt.Exec(med.UUID, med.Title, med.Desciption, med.CreatedAt, med.Owner)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tx.Commit()
 }
+
+// GetMedicine ???
+func (med *Medicine) GetMedicine(db *sql.DB) {
+	rows, err := db.Query("select id, title, desciption, createdAt, owner from med")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(id, name)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// DeleteMedicine deletes Medicine from Database
+func (med *Medicine) DeleteMedicine(db *sql.DB) {
+
+}
+
+func (med *Medicine) CreateMedicineTable(db *sql.DB) {
+	sqlStmt := `
+	create table med (id integer not null primary key, title text, desciption text, createdAt date, owner text);
+	delete from med;
+	`
+
+	_, err := db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
+}
+
+// CreateDataBase creates a SqLite Database
+func CreateDataBase() (*sql.DB, error) {
+	// Für Debug
+	os.Remove("./med.db")
+	db, err := sql.Open("sqlite3", "./med.db")
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+// func trash() {
+// 	stmt, err = db.Prepare("select name from med where id = ?")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer stmt.Close()
+// 	var name string
+// 	err = stmt.QueryRow("3").Scan(&name)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	fmt.Println(name)
+
+// 	_, err = db.Exec("delete from med")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	_, err = db.Exec("insert into med(id, name) values(1, 'med'), (2, 'bar'), (3, 'baz')")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	rows, err = db.Query("select id, name from med")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer rows.Close()
+// 	for rows.Next() {
+// 		var id int
+// 		var name string
+// 		err = rows.Scan(&id, &name)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		fmt.Println(id, name)
+// 	}
+// 	err = rows.Err()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/WirvsVirus-DeMed/backend/db"
 )
@@ -9,5 +11,15 @@ import (
 func main() {
 	fmt.Println("DB TEST")
 
-	db.CreateMedicine()
+	med := &db.Medicine{"1", "1", "1", time.Now(), "1"}
+	db, err := db.CreateDataBase()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	med.CreateMedicineTable(db)
+	med.AddMedicine(db)
+	med.GetMedicine(db)
+	db.Close()
 }

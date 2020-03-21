3a827fa9877dfb679a7f491c3c89247985dc6151
Backerich <backerich@protonmail.com>
2020-03-21 20:23:32 +0100

    Clean and peer CRUD

--- db/db.go
+++ db/db.go
@@ -2,148 +2,19 @@ package db
 
 import (
 	"database/sql"
-	"fmt"
-	"log"
-	"net"
 	"os"
-	"time"
-
-	_ "github.com/mattn/go-sqlite3" // Needs to be like this
 )
 
-// Medicine is the stuff that makes you healthy
-type Medicine struct {
-	UUID        string    `json:"uuid"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	CreatedAt   time.Time `json:"createdAt"`
-	Owner       string    `json:"owner"`
-	Amount      int       `json:"amount"`
-	Pzn         int       `json:"pzn"`
+// RemoveDataBase deletes the data base med
+func RemoveDataBase() {
+	os.Remove("./med.db")
 }
 
-// Packet for transmitting between Peers
-type Packet struct {
-	UUID string
-	Type string
-	data []byte
-}
-
-// Peer for caching the Doctors office
-type Peer struct {
-	IP       net.IP
-	Port     uint32
-	LastSeen time.Time
-}
-
-// Add adds a Medicine Object to the Database
-func (med *Medicine) Add(db *sql.DB) {
-	tx, err := db.Begin()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	stmt, err := tx.Prepare("insert into med(id, title, description, createdAt, owner, amount, pzn) values(?, ?, ?, ?, ?, ?, ?)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-
-	_, err = stmt.Exec(med.UUID, med.Title, med.Description, med.CreatedAt, med.Owner, med.Amount, med.Pzn)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	tx.Commit()
-}
-
-// Delete Medicine from Database
-func (med *Medicine) Delete(db *sql.DB) {
-	_, err := db.Exec("delete from med where id=?", med.UUID)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-// Update the Medicine object in the Database
-func (med *Medicine) Update(db *sql.DB) {
-	med.Delete(db)
-	med.Add(db)
-}
-
-// get wrapper to find a set of results
-func get(db *sql.DB, query string, searchStr string) ([]*Medicine, error) {
-	// SELECT * FROM table WHERE instr(title, searchStr) > 0 OR instr(description, searchStr) > 0 OR searchStr == CAST(pzn as text)
-	// SELECT * FROM med WHERE instr(title, '3') > 0 OR instr(description, '3') > 0 OR '3' == CAST(pzn as text);
-	// PZN != ID
-	rows, err := db.Query(query, searchStr, searchStr, searchStr)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	var meds []*Medicine
-
-	for rows.Next() {
-		var id string
-		var title string
-		var description string
-		var createdAt time.Time
-		var owner string
-		var amount int
-		var pzn int
-
-		err = rows.Scan(&id, &title, &description, &createdAt, &owner, &amount, &pzn)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		med := &Medicine{id, title, description, createdAt, owner, amount, pzn}
-		meds = append(meds, med)
-		fmt.Println(id, title, description, createdAt, owner, amount, pzn)
-	}
-	err = rows.Err()
-	if err != nil {
-		return nil, err
-	}
-
-	return meds, nil
-}
-
-// Get a specific Medicine from the Database based on the search String or the pzn
-func Get(db *sql.DB, searchStr string) ([]*Medicine, error) {
-	return get(db, "select * from med where instr(title, ?) > 0 OR instr(description, ?) > 0 or ? == cast(pzn as text)", searchStr)
-}
-
-// GetAll all the rows of the Database
-func GetAll(db *sql.DB) ([]*Medicine, error) {
-	return get(db, "select id, title, description, createdAt, owner, amount, pzn from med", "")
-}
-
-// DeleteMedicineTable deletes Medicine from Database
-func DeleteMedicineTable(db *sql.DB) {
-	_, err := db.Exec("delete from med")
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-// CreateMedicineTable creates the Medicine Table
-func CreateMedicineTable(db *sql.DB) {
-	sqlStmt := `
-	create table med (id integer not null primary key, title text, description text, createdAt timestamp, owner text, amount integer, pzn integer);`
-
-	_, err := db.Exec(sqlStmt)
-	if err != nil {
-		log.Printf("%q: %s\n", err, sqlStmt)
-		return
-	}
-}
-
-// CreateDataBase creates a SqLite Database
+// CreateDataBase creates a SqLite3 Database named med.db
 func CreateDataBase() (*sql.DB, error) {
 	// Für Debug
-	os.Remove("./med.db")
+	RemoveDataBase()
+
 	db, err := sql.Open("sqlite3", "./med.db")
 	if err != nil {
 		return nil, err
---
+++ db/peer.go
@@ -0,0 +1,119 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+// Peer for caching the Doctors office
+type Peer struct {
+	ID       int
+	IP       net.IP
+	Port     uint32
+	LastSeen time.Time
+}
+
+// Add adds a Peer Object to the Database
+func (peer *Peer) Add(db *sql.DB) {
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stmt, err := tx.Prepare("insert into peer(id, ip, port, lastSeen) values(?, ?, ?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(peer.ID, peer.IP, peer.Port, peer.LastSeen)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tx.Commit()
+}
+
+// Delete Peer from Database
+func (peer *Peer) Delete(db *sql.DB) {
+	_, err := db.Exec("delete from peer where id=?", peer.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Update the Peer object in the Database
+func (peer *Peer) Update(db *sql.DB) {
+	peer.Delete(db)
+	peer.Add(db)
+}
+
+// get wrapper to find a set of results
+func getPeers(db *sql.DB, query string, searchStr string) ([]*Peer, error) {
+	// SELECT * FROM table WHERE instr(title, searchStr) > 0 OR instr(description, searchStr) > 0 OR searchStr == CAST(pzn as text)
+	// SELECT * FROM peer WHERE instr(title, '3') > 0 OR instr(description, '3') > 0 OR '3' == CAST(pzn as text);
+
+	rows, err := db.Query(query, searchStr, searchStr, searchStr)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var peers []*Peer
+
+	for rows.Next() {
+		var id int
+		var ip net.IP
+		var port uint32
+		var lastSeen time.Time
+
+		err = rows.Scan(&id, &ip, &port, &lastSeen)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		peer := &Peer{id, ip, port, lastSeen}
+		peers = append(peers, peer)
+		fmt.Println(id, ip, port, lastSeen)
+	}
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return peers, nil
+}
+
+// TODO:
+// Get a specific peer from the Database based on the search String or the pzn
+// func Get(db *sql.DB, searchStr string) ([]*Peer, error) {
+// 	return getPeers(db, "select * from peer where instr(title, ?) > 0 OR instr(description, ?) > 0 or ? == cast(pzn as text)", searchStr)
+// }
+
+// GetAllPeers all the rows of the Database
+func GetAllPeers(db *sql.DB) ([]*Peer, error) {
+	return getPeers(db, "select * from peer", "")
+}
+
+// DeletePeerTable deletes Peerbacke from Database
+func DeletePeerTable(db *sql.DB) {
+	_, err := db.Exec("delete from peer")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// CreatePeerTable creates the Peer Table
+func CreatePeerTable(db *sql.DB) {
+	sqlStmt := `
+	create table peer (id integer not null primary key, ip blob, port integer, lastSeen timestamp);`
+
+	_, err := db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
+}

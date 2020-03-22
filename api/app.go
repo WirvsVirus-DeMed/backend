d0122fb941dc8fb93a2b68281affef935e2147ad
Backerich <25672920+Backerich@users.noreply.github.com>
2020-03-22 11:01:27 +0100

    Actions schöner

--- api/app.go
+++ api/app.go
@@ -10,12 +10,26 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+type Action func(msg []byte) []byte
+
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
 
 func Api() {
+	// TODO Action hinzufügen
+	actions := map[string]Action{
+		"ProvideMedRessourceRequest":    HandleBackendStateReq,
+		"SearchMedRessourceRequest":     HandleBackendStateReq,
+		"SearchMedRessourceResponse":    HandleBackendStateReq,
+		"RequestMedRessourceRequest":    HandleBackendStateReq,
+		"RequestMedRessourceResponse":   HandleBackendStateReq,
+		"BackendStateRequest":           HandleBackendStateReq,
+		"ChangeMedRessourceRequest":     HandleBackendStateReq,
+		"IncommingMedRessourceResponse": HandleBackendStateReq,
+	}
+
 	http.HandleFunc("/websocket", func(w http.ResponseWriter, r *http.Request) {
 		conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
 
@@ -34,70 +48,80 @@ func Api() {
 				log.Fatal(err)
 			}
 
-			// Silly and not Clean but Golang has no fricking Generics
-			if packet.Type == "ProvideMedRessourceRequest" {
-				var specpacket model.ProvideMedRessourceRequest
-				err = json.Unmarshal(msg, &specpacket)
+			for key, value := range actions {
+				if key == packet.Type {
+					jrep := value(msg)
 
-				if err != nil {
-					log.Fatal(err)
+					if err = conn.WriteMessage(msgType, jrep); err != nil {
+						return
+					}
 				}
-				fmt.Println(specpacket)
-			} else if packet.Type == "SearchMedRessourceRequest" {
-				var specpacket model.SearchMedRessourceRequest
-				err = json.Unmarshal(msg, &specpacket)
-
-				if err != nil {
-					log.Fatal(err)
-				}
-				fmt.Println(specpacket)
-			} else if packet.Type == "SearchMedRessourceResponse" {
-				var specpacket model.SearchMedRessourceResponse
-				err = json.Unmarshal(msg, &specpacket)
-
-				if err != nil {
-					log.Fatal(err)
-				}
-				fmt.Println(specpacket)
-			} else if packet.Type == "RequestMedRessourceRequest" {
-				var specpacket model.RequestMedRessourceRequest
-				err = json.Unmarshal(msg, &specpacket)
-
-				if err != nil {
-					log.Fatal(err)
-				}
-				fmt.Println(specpacket)
-			} else if packet.Type == "RequestMedRessourceResponse" {
-				var specpacket model.RequestMedRessourceResponse
-				err = json.Unmarshal(msg, &specpacket)
-
-				if err != nil {
-					log.Fatal(err)
-				}
-				fmt.Println(specpacket)
-			} else if packet.Type == "BackendStateRequest" {
-				jrep := HandleBackendStateReq(msg)
-
-				if err = conn.WriteMessage(msgType, jrep); err != nil {
-					return
-				}
-			} else if packet.Type == "ChangeMedRessourceRequest" {
-				var specpacket model.ChangeMedRessourceRequest
-				err = json.Unmarshal(msg, &specpacket)
-
-				if err != nil {
-					log.Fatal(err)
-				}
-				fmt.Println(specpacket)
-			} else if packet.Type == "IncommingMedRessourceResponse" {
-				var specpacket model.IncommingMedRessourceResponse
-				err = json.Unmarshal(msg, &specpacket)
-
-				if err != nil {
-					log.Fatal(err)
-				}
-				fmt.Println(specpacket)
 			}
+
+			// Silly and not Clean but Golang has no fricking Generics
+			// if packet.Type == "ProvideMedRessourceRequest" {
+			// 	var specpacket model.ProvideMedRessourceRequest
+			// 	err = json.Unmarshal(msg, &specpacket)
+
+			// 	if err != nil {
+			// 		log.Fatal(err)
+			// 	}
+			// 	fmt.Println(specpacket)
+			// } else if packet.Type == "SearchMedRessourceRequest" {
+			// 	var specpacket model.SearchMedRessourceRequest
+			// 	err = json.Unmarshal(msg, &specpacket)
+
+			// 	if err != nil {
+			// 		log.Fatal(err)
+			// 	}
+			// 	fmt.Println(specpacket)
+			// } else if packet.Type == "SearchMedRessourceResponse" {
+			// 	var specpacket model.SearchMedRessourceResponse
+			// 	err = json.Unmarshal(msg, &specpacket)
+
+			// 	if err != nil {
+			// 		log.Fatal(err)
+			// 	}
+			// 	fmt.Println(specpacket)
+			// } else if packet.Type == "RequestMedRessourceRequest" {
+			// 	var specpacket model.RequestMedRessourceRequest
+			// 	err = json.Unmarshal(msg, &specpacket)
+
+			// 	if err != nil {
+			// 		log.Fatal(err)
+			// 	}
+			// 	fmt.Println(specpacket)
+			// } else if packet.Type == "RequestMedRessourceResponse" {
+			// 	var specpacket model.RequestMedRessourceResponse
+			// 	err = json.Unmarshal(msg, &specpacket)
+
+			// 	if err != nil {
+			// 		log.Fatal(err)
+			// 	}
+			// 	fmt.Println(specpacket)
+			// } else if packet.Type == "BackendStateRequest" {
+			// 	jrep := HandleBackendStateReq(msg)
+
+			// 	if err = conn.WriteMessage(msgType, jrep); err != nil {
+			// 		return
+			// 	}
+			// } else if packet.Type == "ChangeMedRessourceRequest" {
+			// 	var specpacket model.ChangeMedRessourceRequest
+			// 	err = json.Unmarshal(msg, &specpacket)
+
+			// 	if err != nil {
+			// 		log.Fatal(err)
+			// 	}
+			// 	fmt.Println(specpacket)
+			// } else if packet.Type == "IncommingMedRessourceResponse" {
+			// 	var specpacket model.IncommingMedRessourceResponse
+			// 	err = json.Unmarshal(msg, &specpacket)
+
+			// 	if err != nil {
+			// 		log.Fatal(err)
+			// 	}
+			// 	fmt.Println(specpacket)
+			// }
 
 			// med := []db.Medicine{}
 			// rep := &Packet{1, -1, "BackendStateResponse", }

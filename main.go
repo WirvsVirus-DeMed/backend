6c4068f5171576eed1a208573a402edc01de696a
Backerich <25672920+Backerich@users.noreply.github.com>
2020-03-22 21:47:25 +0100

    main stuff

--- main.go
+++ main.go
@@ -1,61 +1,51 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"github.com/WirvsVirus-DeMed/backend/db"
-	"github.com/WirvsVirus-DeMed/backend/node"
-	"log"
-	"net"
-	"os"
-	"strconv"
-	"strings"
-	"time"
-)
+import "github.com/WirvsVirus-DeMed/backend/api"
 
 func main() {
-	log.SetOutput(os.Stdout)
+	// log.SetOutput(os.Stdout)
 
-	_, _ = db.CreateDataBase()
+	// _, _ = db.CreateDataBase()
 
-	client, _ := strconv.Atoi(os.Args[1])
+	// client, _ := strconv.Atoi(os.Args[1])
 
-	fmt.Println("Hello World!")
+	// fmt.Println("Hello World!")
 
-	n := node.Node{}
-	n.Init(
-		"certs/client"+strconv.Itoa(client)+".crt",
-		"certs/client"+strconv.Itoa(client)+".key",
-		"certs/rootCA.crt",
-		"client"+strconv.Itoa(client),
-		uint32(10011+client),
-		100,
-		100,
-		8,
-		10*time.Second)
+	// n := node.Node{}
+	// n.Init(
+	// 	"certs/client"+strconv.Itoa(client)+".crt",
+	// 	"certs/client"+strconv.Itoa(client)+".key",
+	// 	"certs/rootCA.crt",
+	// 	"client"+strconv.Itoa(client),
+	// 	uint32(10011+client),
+	// 	100,
+	// 	100,
+	// 	8,
+	// 	10*time.Second)
 
-	go n.Listen()
-	go n.HandleMessages()
-	go n.BroadcastSender()
-	go n.PeerDiscovery()
-	go n.TidyMedicineOffersRoutine()
+	// go n.Listen()
+	// go n.HandleMessages()
+	// go n.BroadcastSender()
+	// go n.PeerDiscovery()
+	// go n.TidyMedicineOffersRoutine()
 
-	if client != 0 {
-		n.Connect(net.IPv4(127, 0, 0, 1), 10011)
-	}
+	// if client != 0 {
+	// 	n.Connect(net.IPv4(127, 0, 0, 1), 10011)
+	// }
 
-	for {
-		reader := bufio.NewReader(os.Stdin)
-		text, _ := reader.ReadString('\n')
-		if strings.ToLower(text) == "d\n" {
-			log.Printf("[d] CurrentConnections: %v\n", n.Clients.Len())
-		} else if strings.ToLower(text) == "unban\n" {
+	// for {
+	// 	reader := bufio.NewReader(os.Stdin)
+	// 	text, _ := reader.ReadString('\n')
+	// 	if strings.ToLower(text) == "d\n" {
+	// 		log.Printf("[d] CurrentConnections: %v\n", n.Clients.Len())
+	// 	} else if strings.ToLower(text) == "unban\n" {
 
-			for e := n.PeerBlackList.Front(); e != nil; e = e.Next() {
-				n.PeerBlackList.Remove(e)
-			}
-		} else if strings.ToLower(text) == "q\n" {
-			return
-		}
-	}
+	// 		for e := n.PeerBlackList.Front(); e != nil; e = e.Next() {
+	// 			n.PeerBlackList.Remove(e)
+	// 		}
+	// 	} else if strings.ToLower(text) == "q\n" {
+	// 		return
+	// 	}
+	// }
+	api.Api()
 }
